c045cb677e1e5df1564d900ee3d2411a3dcbd101
Harsat <121182309+HarsatMKad@users.noreply.github.com>
2023-02-13 14:58:50 +0700

    created more documents and singletone

--- code.cs
+++ code.cs
@@ -4,42 +4,167 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1 {
-  class Document {
+namespace ConsoleApp1
+{
+  class Document
+  {
     string Name;
     string Author;
     string Tags;
     string Topic;
     string PathToFile;
-    public void Read(string ReadName, string ReadAuthor, string ReadTags, string ReadTopic, string ReadPathToFile) {
-      Name = ReadName;
-      Author = ReadAuthor;
-      Tags = ReadTags;
-      Topic = ReadTopic;
-      PathToFile = ReadPathToFile;
+
+    public virtual void GetInfo() {
+      Console.WriteLine("введите Имя, Автора, Ключевые слова, Тему, Путь к файлу: ");
+      Name = Convert.ToString(Console.ReadLine());
+      Author = Convert.ToString(Console.ReadLine());
+      Tags = Convert.ToString(Console.ReadLine());
+      Topic = Convert.ToString(Console.ReadLine());
+      PathToFile = Convert.ToString(Console.ReadLine());
     }
-    public void Display() {
+
+    public virtual void Display()
+    {
       Console.WriteLine(Name + " " + Author + " " + Tags + " " + Topic + " " + PathToFile);
     }
   }
 
-class Program {
-      static void Main(string[] args) {
+  class WordDocument : Document
+  {
+    int WordsCount;
 
-        Document NewDocument = new Document();
-        string RN, RA, RTa, RTo, RP;
+    public override void GetInfo()
+    {
+      base.GetInfo();
+      Console.WriteLine("укажите количество слов: ");
+      WordsCount = Convert.ToInt32(Console.ReadLine());
+    }
 
-        Console.WriteLine("введите Имя, Автора, Ключевые слова, Тему, Путь к файлу: ");
-        RN = Convert.ToString(Console.ReadLine());
-        RA = Convert.ToString(Console.ReadLine());
-        RTa = Convert.ToString(Console.ReadLine());
-        RTo = Convert.ToString(Console.ReadLine());
-        RP = Convert.ToString(Console.ReadLine());
+    public override void Display()
+    {
+      base.Display();
+      Console.WriteLine(" " + WordsCount);
+    }
+  }
 
-        NewDocument.Read(RN, RA, RTa, RTo, RP);
-        NewDocument.Display();
+  class PDFDocument : Document
+  {
+    int PagesCount;
 
-		Console.ReadKey();
-	  }
-   }
+    public override void GetInfo()
+    {
+      base.GetInfo();
+      Console.WriteLine("укажите количество страниц: ");
+      PagesCount = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public override void Display()
+    {
+      base.Display();
+      Console.WriteLine(" " + PagesCount);
+    }
+  }
+
+  class ExcelDocument : Document
+  {
+    int LineCount, ColumnCount;
+
+    public override void GetInfo()
+    {
+      base.GetInfo();
+      Console.WriteLine("укажите количество сток и столбцов: ");
+      LineCount = Convert.ToInt32(Console.ReadLine());
+      ColumnCount = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public override void Display()
+    {
+      base.Display();
+      Console.WriteLine(" " + LineCount + " " + ColumnCount);
+    }
+  }
+
+  class TXTDocument : Document
+  {
+    int SymbolsCount;
+
+    public override void GetInfo()
+    {
+      base.GetInfo();
+      Console.WriteLine("укажите количество сток и столбцов: ");
+      SymbolsCount = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public override void Display()
+    {
+      base.Display();
+      Console.WriteLine(" " + SymbolsCount);
+    }
+  }
+
+  class HTMLDocument : Document
+  {
+    int webPageTitle;
+
+    public override void GetInfo()
+    {
+      base.GetInfo();
+      Console.WriteLine("укажите количество сток и столбцов: ");
+      webPageTitle = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public override void Display()
+    {
+      base.Display();
+      Console.WriteLine(" " + webPageTitle);
+    }
+  }
+
+  public class Singleton
+  {
+    public static Singleton Instance
+    {
+      get
+      {
+        if (instance == null) instance = new Singleton();
+        return instance;
+      }
+    }
+
+    public void DocumentOutputMethod() 
+    {
+      string DocumentType;
+
+      Console.WriteLine("укажите необходимый тип документа(Doc, Excel): ");
+      DocumentType = Convert.ToString(Console.ReadLine());
+
+      switch (DocumentType)
+      {
+        case ("Doc"):
+          Document Doc = new Document();
+          Doc.GetInfo();
+          Doc.Display();
+          break;
+        case ("Excel"):
+          ExcelDocument ExcelDoc = new ExcelDocument();
+          ExcelDoc.GetInfo();
+          ExcelDoc.Display();
+          break;
+        default:
+          Console.WriteLine("неверный тип документа");
+          break;
+      }
+    }
+    private Singleton() { }
+    private static Singleton instance;
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Singleton.Instance.DocumentOutputMethod();
+      Console.ReadKey();
+    }
+  }
 }
